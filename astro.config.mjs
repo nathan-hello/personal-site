75ee7bb47fc5497ee9ad5c680f141db610148441
nate <16102404+nathan-hello@users.noreply.github.com>
2024-01-06 19:12:20 +0000

    added back tailwind. not sure when that got removed.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
-
 import mdx from "@astrojs/mdx";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site:"https://nathan-hello.com",
+  site: "https://nathan-hello.com",
   output: "hybrid",
   adapter: vercel({
     webAnalytics: false
   }),
-  integrations: [mdx()]
+  integrations: [mdx(), tailwind()]
 });
