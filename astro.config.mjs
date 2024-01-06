c2f67ab6552a4e3156b50d916bef28feb2cc9f98
nate <16102404+nathan-hello@users.noreply.github.com>
2024-01-06 19:18:29 +0000

    turning off tailwind base styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,5 +11,7 @@ export default defineConfig({
   adapter: vercel({
     webAnalytics: false
   }),
-  integrations: [mdx(), tailwind()]
+  integrations: [mdx(), tailwind({
+    applyBaseStyles: false
+  })]
 });
