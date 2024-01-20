14e2b66a5a89c8b73e6922b17cef4fe1e0eb7525
nate <16102404+nathan-hello@users.noreply.github.com>
2024-01-20 17:09:10 +0000

    moving around images, moving accessibility from consts.ts to the blogposts. idk why it wasn't that obvious

--- src/types.ts
+++ src/types.ts
@@ -1,39 +1,40 @@
-import type { AstroInstance, MDXInstance } from 'astro'
-import type { AstroComponentFactory } from 'astro/runtime/server/index.js'
-import type { ImageAccessibility } from './consts'
+import type { AstroInstance, MDXInstance } from 'astro';
+import type { AstroComponentFactory } from 'astro/runtime/server/index.js';
+import type { ImageAccessibility } from './consts';
 
 export type BlogDetails = {
-    title: string
-    date: string
-    author: string
-    overrideHref?: string
-    overrideLayout?: boolean
-    description?: string
-    image?: string | string[]
-    tags?: string[]
-    hidden?: boolean
-}
+    title: string;
+    date: string;
+    author: string;
+    overrideHref?: string;
+    overrideLayout?: boolean;
+    description?: string;
+    image?: string | string[];
+    tags?: string[];
+    aria?: { [x: string]: ImageAccessibility; };
+    hidden?: boolean;
+};
 
 export type Image = {
-    url: string
-    size: string
-    ext: string
-    filename: string
-    fullname: string
-    accessibility: ImageAccessibility
-}
+    url: string;
+    size: string;
+    ext: string;
+    filename: string;
+    fullname: string;
+    accessibility: ImageAccessibility;
+};
 
 export type Post = BlogDetails & {
-    id: number
-    globbedImgs: Image[]
-    relativeUrl: string
-    absoluteUrl: string
-    dateObj: Date
-    Component: AstroComponentFactory
-}
+    id: number;
+    globbedImgs: Image[];
+    relativeUrl: string;
+    absoluteUrl: string;
+    dateObj: Date;
+    Component: AstroComponentFactory;
+};
 
 export type BlogAstro = AstroInstance & {
-    details: BlogDetails
-}
+    details: BlogDetails;
+};
 
-export type BlogMdx = MDXInstance<BlogDetails>
+export type BlogMdx = MDXInstance<BlogDetails>;
--- src/utils/glob.ts
+++ src/utils/glob.ts
@@ -1,44 +1,44 @@
-import { AttributeAccessibilities, type PossibleAuthors } from '@/consts'
+import { CoversAccessibility, type PossibleAuthors } from '@/consts';
 import {
   type BlogAstro,
   type BlogMdx,
   type BlogDetails,
   type Post,
   type Image,
-} from '@/types'
+} from '@/types';
 import {
   extractMetadata,
   formatBytes,
   parseAuthorName,
-} from './parseMetadata'
-import type { AstroComponentFactory } from 'astro/runtime/server/index.js'
-import fs from 'fs'
-import path from 'path'
+} from './parseMetadata';
+import type { AstroComponentFactory } from 'astro/runtime/server/index.js';
+import fs from 'fs';
+import path from 'path';
 
-export const globImages = async (imgs: string[]): Promise<Image[]> => {
+export const globImages = async (imgs: string[], year: string): Promise<Image[]> => {
   const globber = import.meta.glob('/public/**/*.{jpg,gif,png,jpeg,bmp,webp}', {
     as: 'url',
-  })
+  });
 
-  let images: Image[] = []
+  let images: Image[] = [];
 
   for (const img of imgs) {
-    const i = `/public/images/${img}`
-    const url = await globber[i]()
+    const i = `/public/images/covers/${year}/${img}`;
+    const url = await globber[i]();
 
-    const fsPath = `.${i}`
-    const size = fs.statSync(fsPath).size
-    const ext = path.extname(fsPath)
-    const file = path.basename(fsPath, path.extname(fsPath))
-    const urlNoPublic = url.slice('/public'.length)
-    console.log("withpublic", url)
-    console.log("nopublic", urlNoPublic)
+    const fsPath = `.${i}`;
+    const size = fs.statSync(fsPath).size;
+    const ext = path.extname(fsPath);
+    const file = path.basename(fsPath, path.extname(fsPath));
+    const urlNoPublic = url.slice('/public'.length);
+    // console.log("withpublic", url);
+    // console.log("nopublic", urlNoPublic);
 
     if (!url || !urlNoPublic) {
-      throw new Error(`ERROR: ${url} undefined from ${imgs}`)
+      throw new Error(`ERROR: ${url} undefined from ${imgs}`);
     }
 
-    const accessibility = AttributeAccessibilities[img] || {}
+    const accessibility = CoversAccessibility[img] || {};
 
     images.push({
       size: formatBytes(size),
@@ -47,65 +47,65 @@ export const globImages = async (imgs: string[]): Promise<Image[]> => {
       filename: file,
       fullname: `${file}${ext}`,
       accessibility: accessibility
-    })
+    });
   }
-  return images
-}
+  return images;
+};
 
 export type RGlobBlogs = {
   params: {
-    post: string
-  }
+    post: string;
+  };
   props: {
-    c: Post
-  }
-}
+    c: Post;
+  };
+};
 
 export async function globBlogs(
   limit: number | undefined,
   author: PossibleAuthors | undefined,
   hideHidden: boolean | undefined
 ): Promise<RGlobBlogs[]> {
-  let combined: Post[] = []
-  const interim: ReturnType<typeof extractMetadata>[] = []
-  const blogs = import.meta.glob<BlogAstro>('/src/blog/**/*.astro')
+  let combined: Post[] = [];
+  const interim: ReturnType<typeof extractMetadata>[] = [];
+  const blogs = import.meta.glob<BlogAstro>('/src/blog/**/*.astro');
 
-  let count = 100001
+  let count = 100001;
 
   for (const post in blogs) {
-    const f = await blogs[post]()
-    const g = extractMetadata(f)
-    interim.push(g)
+    const f = await blogs[post]();
+    const g = extractMetadata(f);
+    interim.push(g);
   }
 
-  const mdxs = import.meta.glob<BlogMdx>('/src/blog/**/*.mdx')
+  const mdxs = import.meta.glob<BlogMdx>('/src/blog/**/*.mdx');
 
   for (const post in mdxs) {
-    const f = await mdxs[post]()
-    const g = extractMetadata(f)
-    interim.push(g)
+    const f = await mdxs[post]();
+    const g = extractMetadata(f);
+    interim.push(g);
   }
 
-  interim.sort((a, b) => {return a.dateObj.getTime() - b.dateObj.getTime()})
+  interim.sort((a, b) => { return a.dateObj.getTime() - b.dateObj.getTime(); });
 
   for (const p of interim) {
-    const id = count
-    count++
-    let href
+    const id = count;
+    count++;
+    let href;
     if (p.details.overrideHref) {
-      href = p.details.overrideHref
+      href = p.details.overrideHref;
     } else {
       // href = generateHref(p.details.date, id)
-      href = `p/${id}`
+      href = `p/${id}`;
     }
 
-    let imgs: Image[] = []
+    let imgs: Image[] = [];
     if (typeof p.details.image === 'string') {
-      imgs = await globImages([p.details.image])
+      imgs = await globImages([p.details.image], p.dateObj.getFullYear().toString());
     }
 
     if (Array.isArray(p.details.image)) {
-      imgs = await globImages(p.details.image)
+      imgs = await globImages(p.details.image, p.dateObj.getFullYear().toString());
     }
 
     combined.push({
@@ -116,26 +116,26 @@ export async function globBlogs(
       relativeUrl: href,
       absoluteUrl: `${p.details.author}/${href}`,
       globbedImgs: imgs,
-      
-    })
+
+    });
   }
 
-  combined = combined.sort((a, b) => b.dateObj.getTime() - a.dateObj.getTime())
+  combined = combined.sort((a, b) => b.dateObj.getTime() - a.dateObj.getTime());
 
   if (author) {
-    combined = combined.filter((c) => parseAuthorName(c.author) === author)
+    combined = combined.filter((c) => parseAuthorName(c.author) === author);
   }
   if (limit) {
-    combined = combined.slice(0, limit)
+    combined = combined.slice(0, limit);
   }
   if (hideHidden) {
-    combined = combined.filter((c) => c.hidden !== true)
+    combined = combined.filter((c) => c.hidden !== true);
   }
 
   return combined.map((c) => {
     return {
       params: { post: c.relativeUrl },
       props: { c },
-    }
-  })
+    };
+  });
 }
