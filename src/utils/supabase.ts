ddc9b931f46ff2faf5eef9256b0f3167c977149d
nathan-hello <natey3k@gmail.com>
2024-01-23 10:29:08 -0500

    supabase db creds not necessary for build/run

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -3,69 +3,72 @@ import "dotenv/config";
 import type { Database } from "./supabase.types";
 import type { Post } from "@/types";
 
+let s: ReturnType<typeof createClient<Database>>
 if (!process.env.DB_URL || !process.env.DB_KEY) {
-    throw new Error(`No URL or key provided: ${JSON.stringify(process.env)}`);
+  // throw new Error(`No URL or key provided: ${JSON.stringify(process.env)}`);
+  console.error((`No URL or key provided: ${JSON.stringify(process.env)}`))
+
+} else {
+  s = createClient<Database>(process.env.DB_URL, process.env.DB_KEY);
 }
 
-const s = createClient<Database>(process.env.DB_URL, process.env.DB_KEY);
-
 export async function selectBlogById(id: number) {
-    const { data, error } = await s.from("blogs").select().eq("id", id).limit(1).single();
-    if (error !== null) {
-        if (error.code !== "PGRST116") {
-            throw new Error(JSON.stringify(error));
-        }
-        console.error(`blog with id ${id} not found`);
+  const { data, error } = await s.from("blogs").select().eq("id", id).limit(1).single();
+  if (error !== null) {
+    if (error.code !== "PGRST116") {
+      throw new Error(JSON.stringify(error));
     }
-    return data;
+    console.error(`blog with id ${id} not found`);
+  }
+  return data;
 }
 
 export async function insertBlog(p: Post) {
-    const f = await s.from("blogs").insert({ author: p.author, created_at: p.dateObj.toISOString(), id: p.id, url: p.absoluteUrl });
-    if (f.error !== null) {
-        if (f.error.code !== "23505") {
-            throw new Error(JSON.stringify(f.error));
-        }
-        console.error(`blog with id ${p.id} is alrady in db`);
+  const f = await s.from("blogs").insert({ author: p.author, created_at: p.dateObj.toISOString(), id: p.id, url: p.absoluteUrl });
+  if (f.error !== null) {
+    if (f.error.code !== "23505") {
+      throw new Error(JSON.stringify(f.error));
     }
-    return f.status;
+    console.error(`blog with id ${p.id} is alrady in db`);
+  }
+  return f.status;
 }
 
 export async function selectBlogs() {
-    const { data, error } = await s.from('blogs').select();
-    if (data === null) {
-        throw new Error("DB returned no blogs");
-    }
-    if (error !== null) {
-        throw new Error(JSON.stringify(error));
-    }
-    return data;
+  const { data, error } = await s.from('blogs').select();
+  if (data === null) {
+    throw new Error("DB returned no blogs");
+  }
+  if (error !== null) {
+    throw new Error(JSON.stringify(error));
+  }
+  return data;
 }
 
 
 export async function createComment({ author, blogId, body }: { author: string, body: string; blogId: number; }) {
-    const { data, error } = await s.from("comments").insert({ author: author, blog_id: blogId, body: body }).select().maybeSingle();
-    if (error !== null) {
-        console.error(error);
-    }
-    return data;
+  const { data, error } = await s.from("comments").insert({ author: author, blog_id: blogId, body: body }).select().maybeSingle();
+  if (error !== null) {
+    console.error(error);
+  }
+  return data;
 }
 
 export async function selectCommentsFromBlogId(id: number) {
-    const { data, error } = await s.from('comments').select('author, body, created_at').eq('blog_id', id);
-    if (error !== null) {
-        console.error(error);
-    }
-    return data;
+  const { data, error } = await s.from('comments').select('author, body, created_at').eq('blog_id', id);
+  if (error !== null) {
+    console.error(error);
+  }
+  return data;
 }
 
 export async function insertComment(body: string, author: string, blogId: number) {
-    const { data, error } = await s.from("comments").insert({ blog_id: blogId, body: body, author: author }).limit(1).select().single();
+  const { data, error } = await s.from("comments").insert({ blog_id: blogId, body: body, author: author }).limit(1).select().single();
 
-    if (error !== null) {
-        console.error(error);
-    }
+  if (error !== null) {
+    console.error(error);
+  }
 
-    return data;
+  return data;
 
 } 
