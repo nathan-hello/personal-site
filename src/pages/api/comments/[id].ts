fbc09d10d279aff7c7b7d6f2fa555ec289664260
nathan-hello <natey3k@gmail.com>
2024-03-20 01:42:26 -0400

    moved commentsection to its own component, moved htmx to meta.astro

--- src/pages/api/comments/[id].ts
+++ src/pages/api/comments/[id].ts
@@ -5,13 +5,16 @@ import { db, Comment, eq, NOW } from 'astro:db';
 export const prerender = false;
 
 function escapeHTML(str: string) {
-    return str.replace(/&/g, '&amp;')
+    return str
+        .replace(/&/g, '&amp;')
         .replace(/</g, '&lt;')
         .replace(/>/g, '&gt;')
         .replace(/"/g, '&quot;')
         .replace(/'/g, '&#039;');
 }
+
 export const POST: APIRoute = async (ctx) => {
+    const referer = ctx.request.headers.get("Referer") || '/';
     try {
         const formData = await ctx.request.formData();
         const author = formData.get("author")?.toString() || "Anonymous";
@@ -33,9 +36,6 @@ export const POST: APIRoute = async (ctx) => {
                 parentId: id
             });
 
-        // Respond with a 204 No Content status, indicating success
-        // Use the HX-Redirect header for a client-side redirect, which causes a page reload
-        const referer = ctx.request.headers.get("Referer") || '/';
         return new Response(null, {
             status: 204,
             headers: {
@@ -43,14 +43,24 @@ export const POST: APIRoute = async (ctx) => {
             },
         });
     } catch (error) {
-        console.error("Failed to process form submission", error);
-        return new Response("Server error", { status: 500 });
+        return new Response(`
+            <div>Something went wrong. 
+                 Please let us know what happened by filing a github issue. 
+                 Link to the repo is at the top. 
+                 Here is the error: ${JSON.stringify(error)}
+            </div> 
+        `, {
+            status: 200, headers: { "Content-Type": "text/html" }
+        });
+        ;
     }
 };
 
 export const GET: APIRoute = async (ctx) => {
-    console.log(`GET ${ctx.params}`);
     const id = Number(ctx.params.id);
+    if (!id) {
+        return new Response(null, { status: 404 });
+    }
     const comments = await db.select().from(Comment).where(eq(Comment.parentId, id));
     const divs = comments
         .sort((a, b) => b.created_at.getTime() - a.created_at.getTime())
@@ -58,7 +68,7 @@ export const GET: APIRoute = async (ctx) => {
             return `
           <article class="bg-slate-800 text-white w-fit px-2 py-1 my-2">
             <p class="text-green-600 text-md">
-              ${c.author} - ${c.created_at.toLocaleString("en-GB")}
+              ${c.author} - ${c.created_at.toLocaleString("en-GB")} - id: ${c.id}
             </p>
             <p>${c.body}</p>
           </article>
