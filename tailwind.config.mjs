49517ae22af7c75622622c59adc2a65e4a81578b
nate-llc <natesbusiness321@gmail.com>
2024-04-20 17:38:16 -0400

    refactored natalie's layout, changed colors, added /irc

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,13 +6,17 @@ export default {
       colors: {
         "nat-white-c01": "#b3b8c3",
         "nat-black-c01": "#0c090a",
+        "nat-pink-c01": "#440e38",
+        "nat-green-c01": "#0a241e",
+
+    },
+    
       },
       fontFamily: {
         ibmplexserief: ["IBMPlexSerif", "sans-serif"],
         vcrosdmono: ["VCROSDMono", "sans-serif"],
       },
     },
-  },
   plugins: [],
   corePlugins: {
     preflight: false,
