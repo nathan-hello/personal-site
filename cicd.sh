744b7b5032e617e86c516fb7471a1e35eae97134
nate <natey3k@gmail.com>
2025-03-15 09:08:05 -0400

    pipefail

--- cicd.sh
+++ cicd.sh
@@ -1,5 +1,12 @@
 #/bin/sh
 
+# https://stackoverflow.com/questions/2870992/automatic-exit-from-bash-Shell-script-on-error
+# -e exits on error
+# -u errors on undefined variables
+# -x prints commands before execution,
+# and -o (for option) pipefail exits on command pipe failures
+set -euxo pipefail
+
 dir=${0%/*}
 
 chmod +x ./personal-site
