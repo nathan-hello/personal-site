a03fda468c462ebdd709c097020ba43868e84818
relue <natalierogers048@gmail.com>
2025-03-21 16:22:21 -0400

    server rendered latex

--- render/markdown.go
+++ render/markdown.go
@@ -2,6 +2,9 @@ package render
 
 import (
 	"io"
+	"os"
+	"os/exec"
+	"strings"
 
 	"github.com/gomarkdown/markdown"
 	"github.com/gomarkdown/markdown/ast"
@@ -9,9 +12,67 @@ import (
 	"github.com/gomarkdown/markdown/parser"
 )
 
-func MarkdownRender(md []byte) []byte {
+func latexRender(b []byte) []byte {
+	inFile, err := os.CreateTemp("", "pandoc_input_*.md")
+	if err != nil {
+		return b
+	}
+	defer os.Remove(inFile.Name())
+	if _, err := inFile.Write(b); err != nil {
+		inFile.Close()
+		return b
+	}
+	inFile.Close()
 
-	extensions := parser.CommonExtensions | parser.AutoHeadingIDs | parser.NoEmptyLineBeforeBlock | parser.MathJax 
+	outFile, err := os.CreateTemp("", "pandoc_output_*.html")
+	if err != nil {
+		return b
+	}
+	outFile.Close()
+	defer os.Remove(outFile.Name())
+
+	cmd := exec.Command("pandoc", "-s", inFile.Name(), "-o", outFile.Name(), "--mathml")
+	if err := cmd.Run(); err != nil {
+		return b
+	}
+
+	outBytes, err := os.ReadFile(outFile.Name())
+	if err != nil {
+		return b
+	}
+
+   	outStr := string(outBytes)
+    start := strings.Index(outStr, "<p>")
+    end := strings.Index(outStr, "</body>")
+
+	if start != -1 && end != -1 {
+        return []byte(outStr[start:end])
+	}
+    return []byte(outStr)
+}
+
+func hasInlineLatex(b []byte) bool {
+    for i := 0; i < len(b); i++ {
+        if b[i] == '$' && (i == 0 || b[i-1] != '\\') {
+            for j := i + 1; j < len(b); j++ {
+                if b[j] == '$' && b[j-1] != '\\' {
+                    if j > i+1 {
+                        return true
+                    }
+                    break
+                }
+            }
+        }
+    }
+    return false
+}
+
+func MarkdownRender(md []byte) []byte {
+    if hasInlineLatex(md) {
+        md = latexRender(md)
+    }
+
+    extensions := parser.CommonExtensions | parser.AutoHeadingIDs | parser.NoEmptyLineBeforeBlock
 	p := parser.NewWithExtensions(extensions)
 
 	doc := p.Parse(md)
